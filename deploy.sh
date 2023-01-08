ad03382cc40a2afe82727db088cbbfbe87ada8e4
zhoupc <zhoupc1988@gmail.com>
2023-01-08 11:46:44 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 if git diff --quiet; then 
 	echo "update dami-lab.top"
+	case $HOSTNAME in
+		(damiserver) 
+			hugo 
+			rsync -avzP ./public/* /var/www/html/dami-lab.top --delete
+			echo "done!";;
+		(*)
+			echo $HOSTNAME
+			echo "push to github and deploy the website on lab server"
+			ssh damilab 'cd ~/damilab/github/dami-lab.top/; git pull ; sh deploy.sh'
+	esac
 else
 	echo "no changes"
 fi 
 
-case $HOSTNAME in
-	(damiserver) 
-		hugo 
-		rsync -avzP ./public/* /var/www/html/dami-lab.top --delete
-		echo "done!";;
-	(*)
-		echo $HOSTNAME
-		echo "push to github and deploy the website on lab server"
-		ssh damilab 'cd ~/damilab/github/dami-lab.top/; git pull ; sh deploy.sh'
-esac
+
